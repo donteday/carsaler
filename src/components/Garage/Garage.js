4de0eed676cbd4d8c3619f519337bf0a2f4d1b6e
donteday <meowskyj@yandex.ru>
2023-07-18 14:22:09 +0300

    feat: garage restaling

--- src/components/Garage/Garage.js
+++ src/components/Garage/Garage.js
@@ -1,21 +1,21 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { update, incrementMoney } from '../../redux/store/store'
+import { useSelector } from 'react-redux'
+// import { update, incrementMoney } from '../../redux/store/store'
 import GarageCar from '../GarageCar/GarageCar';
 import './Garage.css'
 
 
 const Garage = () => {
-    const dispatch = useDispatch();
+    // const dispatch = useDispatch();
     const garage = useSelector(state => state.counter.garage);
-    const money = useSelector(state => state.counter.money);
+    // const money = useSelector(state => state.counter.money);
     const garageSpaces = useSelector(state => state.counter.garageSpaces);
-    const placePrice = garageSpaces.length * garageSpaces.length * 100000;   
+    // const placePrice = garageSpaces.length * garageSpaces.length * 100000;   
 
-    function buyGaragePlace() {
-        if ((money - placePrice) <= 7000) return;
-        dispatch(incrementMoney(-placePrice));
-        dispatch(update({ name: 'garageSpaces', source: [...garageSpaces, garageSpaces.length] }))
-    }
+    // function buyGaragePlace() {
+    //     if ((money - placePrice) <= 7000) return;
+    //     dispatch(incrementMoney(-placePrice));
+    //     dispatch(update({ name: 'garageSpaces', source: [...garageSpaces, garageSpaces.length] }))
+    // }
 
     return (<>
         Гараж
@@ -31,9 +31,9 @@ const Garage = () => {
                 </div>
                 : ''
         )}
-        <div className='garage__place'>
+        {/* <div className='garage__place'>
             <span className="link" onClick={buyGaragePlace}>Купить место {placePrice}</span>
-        </div>
+        </div> */}
     </>);
 }
 
--- src/components/GarageCar/GarageCar.js
+++ src/components/GarageCar/GarageCar.js
@@ -11,7 +11,7 @@ const GarageCar = ({ car, index }) => {
     const engineRef = useRef();
     const blackRef = useRef();
     const dmg = useSelector(state => state.counter.garage[index].damage);
-    const saleDuration = car.mileage/5 + car.ratio*100;
+    const saleDuration = car.mileage / 5 + car.ratio * 100;
     function sell() {
         console.log(car.status);
         if (car.status) return;
@@ -33,7 +33,7 @@ const GarageCar = ({ car, index }) => {
             blackRef.current.style.display = 'none';
         }
         engineRef.current.style.transform = 'scale(1.05)';
-        dispatch(addExp(1000));
+        dispatch(addExp(250));
         dispatch(carRepair(index));
     }
     function engineUnScale() {
@@ -41,22 +41,25 @@ const GarageCar = ({ car, index }) => {
     }
 
     return (
-        <div className='car__container'>
-            <div className="market__car garage_car" ref={thisRef}>
-                <div className='market__car-title'>
-                    <span>{car.name}</span>
-                    <img className="market__car-img" src={require(`../../img/${car.img}`)} alt="" style={{ filter: `sepia(${100 - dmg}%)` }} />
-                    <span>{car.status ? `Время продажи: ${saleDuration / 1000}сек` : ''}</span>
+        <div className='car__container car__container-garage'>
+            <div className="garage_car" ref={thisRef}>
+                <div className='garage_car-title'>
+                    <div className='garage__car-info'>
+                        <span>{car.name}</span>
+                        <span>Состояние: {car.damage}</span>
+                        <span>Цена: {car.endPrice}</span>
+                        <span>Пробег: {car.mileage}км</span>
+                        <span style={car.status ? { color: `yellowgreen` } : { color: `red` }}>{car.status ? 'В продаже' : 'Не продается'}</span>
+                        <span>{car.status ? `Время продажи: ${(saleDuration / 1000).toFixed(0)}сек` : ''}</span>
+                    </div>
+                    <div className='garage__car-buttons'>
+                        <span className='link' onClick={sell}>Продать</span>
+                        <span className='link' onClick={onRepair}>Починить</span>
+                    </div>
+                </div>
+                <img className="garage_car-img" src={require(`../../img/shadow.png`)} alt="" style={{ filter: `sepia(${100 - dmg}%)` }} />
+                <img className="garage_car-img" src={require(`../../img/${car.img}`)} alt="" style={{ filter: `sepia(${100 - dmg}%)` }} />
 
-                </div>
-                <div className='market__car-info'>
-                    <span style={car.status ? { color: `yellowgreen` } : { color: `red` }}>{car.status ? 'В продаже' : 'Не продается'}</span>
-                    <span>Состояние: {car.damage}</span>
-                    <span>Цена: {car.endPrice}</span>
-                    <span>Пробег: {car.mileage}км</span>
-                    <span className='link' onClick={sell}>Продать</span>
-                    <span className='link' onClick={onRepair}>Починить</span>
-                </div>
             </div>
             <div className='car__repair' ref={repairRef}>
                 <img className="car__lift" src={require(`../../img/lift_1.png`)} alt="" />
